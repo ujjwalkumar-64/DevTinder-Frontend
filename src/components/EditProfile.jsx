d3a349753e20bb436b418790a2d3082d1dda4d6e
ujjwalkumar-64 <143947231+ujjwalkumar-64@users.noreply.github.com>
2025-01-13 17:40:02 +0530

    ui bug fix

--- src/components/EditProfile.jsx
+++ src/components/EditProfile.jsx
@@ -55,8 +55,8 @@ const EditProfile = ({user}) => {
   return (
      <>
 
-    <div className='flex justify-center'>
-        <div className='my-10 flex justify-center mx-10'>
+    <div className='flex justify-center my-10'>
+        <div className=' flex justify-center mx-10'>
             <div className="card bg-base-300 w-96 shadow-xl">
                 <div className="card-body">
                     <h2 className="card-title justify-center">Edit Profile</h2>
@@ -153,7 +153,7 @@ const EditProfile = ({user}) => {
             </div>
         </div>
                         
-       <div className=' my-10'>
+       <div >
        <Card user={{firstName,lastName,about,skills,age,gender,photoUrl}}/>
        </div>
     </div>
