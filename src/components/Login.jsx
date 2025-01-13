41d3d0d3d3b9ce1ec0c9498635fc372ea83d0f2b
ujjwalkumar-64 <143947231+ujjwalkumar-64@users.noreply.github.com>
2025-01-14 01:47:23 +0530

    login signup merge

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -6,13 +6,16 @@ import { useNavigate } from 'react-router-dom';
 import { BaseUrl } from '../utils/constant';
 
 const Login = () => {
-    const [email,setEmail]= useState("sunny@gmail.com");
-    const [password,setPassword]= useState("Rgipt@123");
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
+    const [email,setEmail] = useState("")
+    const [password,setPassword] = useState("")
+    const [firstName,setFirstName]= useState("")
+    const [lastName,setLastName]= useState("")
+    const [isloggedIn,setIsLoggedIn] = useState(true);
     const [error,setError]= useState("");
 
-    const handleSubmit= async()=>{
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const handleLogin= async()=>{
          try {
             const res= await axios.post(BaseUrl + "/login",{
                 email,
@@ -25,17 +28,69 @@ const Login = () => {
            return  navigate("/")
          } catch (error) {
             setError(error?.response?.data)
-            console.error(error)
+            
          }
     }
+
+    const handleSignup= async()=>{
+        setError("");
+        try {
+            const res= await axios.post(BaseUrl+"/signup",{
+                email,
+                firstName,
+                lastName,
+                password,    
+            },{
+                withCredentials:true
+            })
+            
+            dispatch(addUser(res.data.data));
+            return navigate("/profile")
+        } catch (error) {
+            setError(error?.response?.data || "Something went wrong")
+        }
+    }
+    
+
+
   return (
     <div className='my-10 flex justify-center'>
         <div className="card bg-base-300 w-96 shadow-xl">
             <div className="card-body">
-                <h2 className="card-title justify-center">Login Page</h2>
+                <h2 className="card-title justify-center">{isloggedIn?"Login Page":"Signup Page"}</h2>
+
+                {
+                    !isloggedIn && <>
+
+                    <label className="form-control w-full max-w-xs my-2">
+                        <div className="label">
+                            <span className="label-text">First Name</span>
+                        </div>
+                        <input type="text" 
+                            value={firstName} 
+                            placeholder="Type here" 
+                            className="input input-bordered input-accent w-full max-w-xs"
+                            onChange={(e)=> setFirstName(e.target.value)}
+                            />
+                    </label>
+                    <label className="form-control w-full max-w-xs my-2">
+                        <div className="label">
+                            <span className="label-text">Last Name</span>
+                        </div>
+                        <input type="text" 
+                            value={lastName} 
+                            placeholder="Type here" 
+                            className="input input-bordered input-accent w-full max-w-xs"
+                            onChange={(e)=> setLastName(e.target.value)}
+                            />
+                    </label>
+
+                    </>
+                }
+
                 <label className="form-control w-full max-w-xs my-2">
                     <div className="label">
-                        <span className="label-text">Username</span>
+                        <span className="label-text">Email</span>
                     </div>
                     <input type="email" 
                         value={email} 
@@ -54,10 +109,20 @@ const Login = () => {
                     className="input input-bordered input-accent w-full max-w-xs" 
                     onChange={(e)=>setPassword(e.target.value)}/>
                 </label>
+
                     <p className='text-red-800'>{error}</p>
                     <div className="card-actions justify-center m-2">
-                        <button className="btn btn-accent" onClick={handleSubmit}>Submit</button>
+                        <button className="btn btn-accent" onClick={isloggedIn? handleLogin :  handleSignup}>Submit</button>
                     </div>
+
+                    <p
+                        className="m-auto cursor-pointer py-2"
+                        onClick={() => setIsLoggedIn((value) => !value)}
+                    >
+                        {isloggedIn
+                        ? "New User? Signup Here"
+                        : "Existing User? Login Here"}
+                    </p>
             </div>
         </div>
     </div>
