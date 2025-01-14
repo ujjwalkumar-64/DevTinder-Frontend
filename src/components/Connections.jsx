8bc85e6f5a3382885a89dff83664e288bb991998
ujjwalkumar-64 <143947231+ujjwalkumar-64@users.noreply.github.com>
2025-01-14 17:04:43 +0530

    responsive for mobile

--- src/components/Connections.jsx
+++ src/components/Connections.jsx
@@ -1,7 +1,6 @@
 import axios from 'axios'
 import React, { useEffect } from 'react'
 import { BaseUrl } from '../utils/constant'
-import Card from "./Card"
 import { useDispatch, useSelector } from 'react-redux'
 import { addConnection } from '../utils/connectionSlice'
 
