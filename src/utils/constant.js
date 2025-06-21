cacdfbc514e8877e5bcb20930036fc7f334d25eb
ujjwalkumar-64 <143947231+ujjwalkumar-64@users.noreply.github.com>
2025-06-22 00:02:25 +0530

    constant update

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,3 +1,7 @@
 // export const BaseUrl= "/api";
 
-export const BaseUrl = location.hostname==="localhost"?"http://localhost:3000":"/api"
+// export const BaseUrl = location.hostname==="localhost"?"http://localhost:3000":"/api"
+export const BaseUrl =
+  location.hostname === "localhost"
+    ? "http://localhost:3000"
+    : import.meta.env.VITE_API_URL;
